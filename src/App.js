991f40b96a7d081f6e07550d725f76ea732e1923
KirtikumarKavande <kirtikumar0005@gmail.com>
2023-06-02 17:27:34 +0530

    feat:protected product detail page being accesed by not loged in users

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { useContext } from "react";
 import Home from "./Home/Home";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  redirect,
+  RouterProvider,
+} from "react-router-dom";
 import Album from "./Pages/Album";
 import About from "./Pages/About";
 import Root from "./Root";
@@ -13,36 +17,41 @@ import Product from "./Pages/Product";
 import Login from "./Pages/Login";
 import { LoginToken } from "./Store/Login/LoginContext";
 
-const LoginStatus = localStorage.getItem("idToken");
-let isLoggedIn = false;
-
-const myrouter = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <ErrorShow />,
-    children: [
-      { path: "", element: <Body /> },
-
-      {
-        path: "/:productDetails",
-        element: <Product />,
-      },
-      { path: "about", element: <About /> },
-      { path: "album", element: <Album /> },
-      { path: "contactUs", element: <Contact /> },
-      { path: "login", element: <Login /> },
-
-      { path: "album/:details", element: <AlbumDetails /> },
-    ],
-  },
-]);
-
 const App = () => {
+  const ctxstatus = useContext(LoginToken);
+
+  const myrouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+      errorElement: <ErrorShow />,
+      children: [
+        { path: "", element: <Body /> },
+
+        {
+          path: "/:productDetails",
+          element: !!ctxstatus.token ? <Product /> : <Login />,
+        },
+        { path: "about", element: <About /> },
+        { path: "album", element: <Album /> },
+        { path: "contactUs", element: <Contact /> },
+        {
+          path: "login",
+          element: !!!ctxstatus.token ? (
+            <Login />
+          ) : (
+            <p>you are already looged in</p>
+          ),
+        },
+
+        { path: "album/:details", element: <AlbumDetails /> },
+      ],
+    },
+  ]);
+
   const statusToken = useContext(LoginToken);
   console.log("app", statusToken.token);
 
-  isLoggedIn = !!statusToken;
   return (
     <div>
       <RouterProvider router={myrouter} />
